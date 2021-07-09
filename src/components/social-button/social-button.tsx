5aacd5bc8d773c18a3f62f842993eb2e73f87f78
Rui Lebre <ruilebre@ua.pt>
2021-07-09 14:37:32 +0100

    Added social button component

---
+++ src/components/social-button/social-button.tsx
@@ -0,0 +1,22 @@
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import styles from "./social-button.module.scss";
+
+interface Props {
+  icon: IconDefinition;
+  link: string;
+  newPage?: boolean;
+}
+
+export const SocialButton = ({ icon, link, newPage }: Props) => {
+  return (
+    <a
+      className={styles.btn__social}
+      href={link}
+      target={newPage ? "_blank" : "_parent"}
+    >
+      <FontAwesomeIcon icon={icon} className="w-6 text-gray-500" />
+    </a>
+  );
+};
