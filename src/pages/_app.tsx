d6f047780d23e03e95ee3eb87b964ad3a4b2b821
Rui Lebre <ruilebre@ua.pt>
2021-08-26 14:29:37 +0100

    Added academic, professional and hobbies pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,44 @@
 import '../assets/styles/globals.scss';
 import type { AppProps } from 'next/app';
+import React, { useEffect } from 'react';
+import Head from 'next/head';
+import Layout from '../components/layout';
+import Academic from './academic';
+import Professional from './professional';
+import Hobbies from './hobbies';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    document.documentElement.lang = 'en';
+  }, []);
+
+  let withLayout = false;
+  switch (Component) {
+    case Academic:
+    case Professional:
+    case Hobbies:
+      withLayout = true;
+      break;
+  }
+
+  return (
+    <>
+      <Head>
+        <meta name='ruilebre.com' content='Personal website of Rui Lebre - software engineer.' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <title>Rui Lebre</title>
+      </Head>
+      <>
+        {withLayout ? (
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        ) : (
+          <Component {...pageProps} />
+        )}
+      </>
+    </>
+  );
 }
 
 export default MyApp;
---
+++ src/pages/professional/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Professional = () => {
+  return <div>Professional</div>;
+};
+
+export default Professional;
