ba4cb0c2a7f6065055a64f6779c1bd10e8d771ff
Rui Lebre <rui.andre.lebre@gmail.com>
2023-10-13 21:55:38 +0100

    Add Homarr

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ module.exports = {
         source: '/academic',
         destination: 'https://ruilebre.com/academic',
       },
+      {
+        source: '/homarr',
+        destination: 'https://ruilebre.com/homarr',
+      },
     ];
   },
 };
