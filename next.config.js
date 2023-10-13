d240f1a363779e3c2009464f74e03c2742954d57
Rui Lebre <rui.andre.lebre@gmail.com>
2023-10-13 22:04:14 +0100

    Revert homarr

--- next.config.js
+++ next.config.js
@@ -6,10 +6,6 @@ module.exports = {
         source: '/academic',
         destination: 'https://ruilebre.com/academic',
       },
-      {
-        source: '/homarr',
-        destination: 'https://ruilebre.com/homarr',
-      },
     ];
   },
 };
