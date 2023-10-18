b03cd84af5f362d601cfde461b906c15560a8d66
Rui Lebre <rui.andre.lebre@gmail.com>
2023-10-18 09:21:30 +0100

    Fixed favicon (#6)

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: 'Rui Lebre',
   description: 'Personal website of Rui Lebre - software engineer.',
   viewport: 'width=device-width, initial-scale=1.0',
-  icons: 'logo.svg',
+  icons: '/logo.svg',
 };
 
 export default function RootLayout({
